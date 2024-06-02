895aaa39a34fa6c2a95bd68a795be9107691ea21
FlexoTim <tfmromulus@gmail.com>
2024-06-02 12:27:19 -0400

    Add project files.

---
+++ ftBittleX.cpp
@@ -0,0 +1,17 @@
+//
+// ftBittleX.cpp
+//
+
+#include "ftBittleX.h"
+
+namespace Petoi {
+	namespace ftBittleX {
+
+		TEST(TestCaseName, TestName) {
+			EXPECT_EQ(1, 1);
+			EXPECT_TRUE(true);
+		}
+
+	}	// namespace ftBittleX
+}	// namespace Petoi
+
---
+++ ftBittleX.h
@@ -0,0 +1,13 @@
+//
+// pch.h
+//
+
+#pragma once
+
+#include "gtest/gtest.h"
+#include "serial/serial.h"
+
+namespace Petoi {
+	namespace ftBittleX {
+	}	// namespace ftBittleX
+}	// namespace Petoi
